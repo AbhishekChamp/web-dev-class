f339d04723f0eacdfc471b40238c1f4ed73fe96f
AbhishekChamp <abhishek96.r@gmail.com>
2026-01-27 20:17:26 +0530

    Class-04 Live class

---
+++ class-04/concepts/script.js
@@ -0,0 +1,50 @@
+// Literal and Variable
+// Literal
+console.log("This is class-04");
+
+// variable
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+a = "Javascript";
+console.log(a);
+
+// assignment operator
+let maths = 96;
+let science = 94;
+
+// Arithmetic operator
+console.log(maths + science);
+
+// algebraic
+const total = maths + science;
+console.log(total + 10);
+
+// String Operator
+let firstName = "Nethra";
+let lastName = "Gokul";
+
+console.log(firstName + " " + lastName);
+console.log(firstName.length);
+console.log(firstName.toLocaleUpperCase());
+
+// Conditional operator
+let marks = 54;
+
+console.log(marks > 40);
+
+// Logical operator
+let attendance = 80;
+marks = 60;
+
+console.log(attendance > 85 && marks > 40);
+console.log(attendance > 85 || marks > 40);
+
+// Type comparison
+console.log(typeof 10);
+console.log(typeof "10");
+console.log(10 == "10");
+console.log(10 === "10");
