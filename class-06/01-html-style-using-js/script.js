ddb31dbecd5c081c16cb330293a229b78448871c
AbhishekChamp <abhishek96.r@gmail.com>
2026-02-03 20:14:06 +0530

    class-06 live class

---
+++ class-06/01-html-style-using-js/script.js
@@ -0,0 +1,5 @@
+function changeStyle() {
+    document.getElementById("text").style.fontSize = "24px";
+    document.getElementById("text").style.color = "blue";
+    document.querySelector("#text").style.backgroundColor = "green";
+}
---
+++ class-06/02-html-show-hide-js/script.js
@@ -0,0 +1,15 @@
+function hideVisibility() {
+    document.getElementById("paraVisibility").style.visibility = "hidden";
+}
+
+function showVisibility() {
+    document.getElementById("paraVisibility").style.visibility = "visible";
+}
+
+function hideDisplay() {
+    document.getElementById("paraDisplay").style.display = "none";
+}
+
+function showDisplay() {
+    document.getElementById("paraDisplay").style.display = "block";
+}
